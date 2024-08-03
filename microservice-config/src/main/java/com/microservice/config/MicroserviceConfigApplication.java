3f950ce47c7766d2733e01dedc2ad65db83c4d9d
RickUbb <ricardovalle2000@hotmail.com>
2024-08-03 00:50:53 -0500

    Added initial microservices: Eureka, Config, and Gateway

---
+++ microservice-config/src/main/java/com/microservice/config/MicroserviceConfigApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigApplication.class, args);
+	}
+
+}
